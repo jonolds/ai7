36b55f7217e9bd3917f4056e6994a8d8c23e88ca
Jon Olds <jonolds@gmail.com>
2018-11-30 12:15:20 -0600

    adf

--- src/Mat.java
+++ src/Mat.java
@@ -1,18 +1,17 @@
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 public class Mat extends JonsLib {
-
-	private ArrayList<double[]> data;
+	private ArrayList<double[][]> data;
 	private int cols;
-	
-//	Mat(int rows, int cols) {
-//		data = new ArrayList<>();
-//		ints(0, rows).forEach(i->data.add(new double[cols]));
-//		this.cols = cols;
-//	}
-	
 	Mat() {}
 	
+	Mat(int rows, int cols) {
+		data = new ArrayList<>();
+		ints(0, rows).forEach(i->data.add(new double[cols][4]));
+		this.cols = cols;
+	}
+
 	Mat(int cols) {
 		data = new ArrayList<>();
 		this.cols = cols;
@@ -26,42 +25,40 @@ public class Mat extends JonsLib {
 	
 	void pushRow(double[] newRow) {
 		try {
-			data.add(newRow);
+			double[][] d2 = ints(0, newRow.length).mapToObj(i->ints(0,4).mapToDouble(z->newRow[i]).toArray()).toArray(double[][]::new);
+			data.add(d2);
 		} catch(Exception e) {
-			this.data = new ArrayList<>(cols = newRow.length);
+			this.data = new ArrayList<>();
 			pushRow(newRow);
 		}
+		cols = newRow.length;
 		if(newRow.length != cols) error("pushRow() error: mismatched column size");
 //		data.add(newRow);
 	}
 	
-	void setCol(int col, double d) { ints(0, rows()).forEach(i->row(i)[col] = d); }
-	void setSlotInRow(int slot, int row, double d) {ints(0, cols()).forEach(c->row(row)[c*4 + slot] = d);}
-	void set(int r, int c, int s, double d) { row(r)[4*c + s] = d; }
-	
+//	void setCol(int col, double d) { ints(0, rows()).forEach(i->row(i)[col] = d); }
+//	void setSlotInRow(int slot, int row, double d) {ints(0, cols()).forEach(c->row(row)[c*4 + slot] = d);}
+//	void set(int r, int c, int s, double d) { row(r)[4*c + s] = d; }
 	
 	int rows() { return data.size(); }
-	int cols() { return cols/4; }
+	int cols() { return cols; }
 	
-	double[] row(int rowNum) { return data.get(rowNum); }
-	double[] first() { if(data.isEmpty()) error("Matrix is empty."); return row(0); }
-	double[] last() { if(data.isEmpty()) error("Matrix is empty."); return row(rows()-1); }
-	
+	double[][] row(int rowNum) { return data.get(rowNum); }
+	double[][] first() { if(data.isEmpty()) error("Matrix is empty."); return row(0); }
+	double[][] last() { if(data.isEmpty()) error("Matrix is empty."); return row(rows()-1); }
 
-	
 	void removeRow(int rowNum) {
 		data.remove(rowNum);
 	}
 	
-	
 	void printAll() {
-		for(int i = 0; i < rows(); i++)
-			println(row(i));
+		DecimalFormat df = new DecimalFormat("###.#");
+		for(int r = 0; r < rows(); r++) {
+			println(row(r));
+		}
 	}
 	
 	boolean isEmpty() { return data.isEmpty(); }
-	
-	
 	
 //	double colSum(int colNum) {
 //		if(colNum > cols-1) error("ColSum error: col out of range");
@@ -78,5 +75,4 @@ public class Mat extends JonsLib {
 //	void error(String description) {
 //		throw new IllegalArgumentException(description);
 //	}
-	
 }
--- src/QLearner.java
+++ src/QLearner.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Formatter;
 import java.util.Random;
 
 
@@ -10,22 +11,21 @@ public class QLearner extends JonsLib {
 	static final double epsilon = .05, gamma = .97, ak = .1, pay = 100;
 	static int[] startState;
 
-	
 	void loadMap(String filename) throws IOException {
 		Files.lines(Paths.get(filename)).forEach(ln->{M.pushRow(lnToArr(ln.replace(",", "").toCharArray()));});
 		if(M.isEmpty()) error("Empty data file");
-		ints(0, M.cols()).forEach(c->{if(M.row(0)[4*c + 1] < pay) M.set(0, c, 1, -1);});
-		ints(0, M.cols()).forEach(c->{if(M.last()[4*c + 2] < pay)M.set(M.rows()-1, c, 2, -1);});
-		ints(0, M.rows()).forEach(r->{if(M.row(r)[0] < pay) M.set(r, 0, 0, -1);});
-		ints(0, M.rows()).forEach(r->{if(M.row(r)[M.cols()*4-1] < pay) M.set(r, M.cols()-1, 3, -1);});
+//		ints(0, M.cols()).forEach(c->{if(M.row(0)[4*c + 1] < pay) M.set(0, c, 1, -1);});
+//		ints(0, M.cols()).forEach(c->{if(M.last()[4*c + 2] < pay)M.set(M.rows()-1, c, 2, -1);});
+//		ints(0, M.rows()).forEach(r->{if(M.row(r)[0] < pay) M.set(r, 0, 0, -1);});
+//		ints(0, M.rows()).forEach(r->{if(M.row(r)[M.cols()*4-1] < pay) M.set(r, M.cols()-1, 3, -1);});
+		
+		M.printAll();
+		
 	}
+	Formatter fm = new Formatter();
+	
 	double[] lnToArr(char[] ln) {
-		double[] row = new double[ln.length*4];
-		ints(0, ln.length).forEach(c-> ints(0, 4).forEach(i->row[slot(c, i)] = (ln[c] == '#') ? -1 : (ln[c] == 'G') ? pay : 0));
-		return row;
-	}
-	int slot(int col, int slot) {
-		return 4*col + slot;
+		return ints(0, ln.length).mapToDouble(c-> (ln[c] == '#') ? -1 : (ln[c] == 'G') ? pay : 0.1).toArray();
 	}
 	
 	double Qia(int[] i, int act) {
@@ -34,36 +34,36 @@ public class QLearner extends JonsLib {
 	
 	
 	QLearner() throws IOException {
-		loadMap("in_map.txt");
+		loadMap("small_map.txt");
 		startState = new int[] {M.rows() -1, 0};
-		M.printAll();
+//		M.printAll();
 	}
 	void run(int[] state) {
-		int[] i = state;
-		int act;
-		
-		if(r.nextDouble() < epsilon)
-			act = r.nextInt(4);
-		else {
-			act = 0;
-//			act = ints(0, 4).map(z->getMoveVal(i, z) > getMoveVal(i, act) ? z : act);
-			act = getMoveVal(i, act) == 0.0 ? r.nextInt(4) : act;
-		}
-		i = makeMove(act);
+//		int[] i = state;
+//		int act;
+//		
+//		if(r.nextDouble() < epsilon)
+//			act = r.nextInt(4);
+//		else {
+//			act = 0;
+////			act = ints(0, 4).map(z->getMoveVal(i, z) > getMoveVal(i, act) ? z : act);
+//			act = getMoveVal(i, act) == 0.0 ? r.nextInt(4) : act;
+//		}
+//		i = makeMove(act);
 	}
 
 	public static void main(String[] args) throws IOException {
 		QLearner ql = new QLearner();
 		ql.run(startState);
 	}
-	
-	int[] makeMove(int move) { 
-		return new int[] {99,99}; 
-	}
-	double getMoveVal(int[] state, int move) { 
-		return move; 
-	}
-	boolean isConverged() { 
-		return false; 
-	}
+//	
+//	int[] makeMove(int move) { 
+//		return new int[] {99,99}; 
+//	}
+//	double getMoveVal(int[] state, int move) { 
+//		return move; 
+//	}
+//	boolean isConverged() { 
+//		return false; 
+//	}
 }
